5b68421561838f55eddeb4b6a3706068b988d800
Daniel-Dos <daniel.dias.analistati@gmail.com>
2016-10-22 03:04:21 -0200

    Exemplo FileSystemXmlApplicationContext

--- SpringApplicationContextContainer/src/com/tutorialspoint/springframework/applicationcontextcontainer/MainApp.java
+++ SpringApplicationContextContainer/src/com/tutorialspoint/springframework/applicationcontextcontainer/MainApp.java
@@ -38,10 +38,10 @@ import org.springframework.web.context.support.XmlWebApplicationContext;
  *
  */
 public class MainApp {
-	public static void main(String[] args) {
-		ApplicationContext context = new FileSystemXmlApplicationContext("/src/Beans.xml");
-
-		HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
-		obj.getMessage();
-	}
+ public static void main(String[] args) {
+	ApplicationContext context = new FileSystemXmlApplicationContext("/src/Beans.xml");
+	
+	HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+	obj.getMessage();
+ }
 }
