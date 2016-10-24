0359d571d4efff5b25d1b0e3b4d67ea244241d2c
Daniel-Dos <daniel.dias.analistati@gmail.com>
2016-10-24 02:31:09 -0200

    Atualização.

--- Spring-Bean Post Processors /BeanPostProcessors/src/com/tutorialspoint/springframework/beanpostprocessor/InitHelloWorld.java
+++ Spring-Bean Post Processors /BeanPostProcessors/src/com/tutorialspoint/springframework/beanpostprocessor/InitHelloWorld.java
@@ -36,17 +36,17 @@ import org.springframework.core.Ordered;
  */
 public class InitHelloWorld implements BeanPostProcessor {
 
-	@Override
-	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException { // BeforeInitialization
-		// TODO Auto-generated method stub
-		System.out.println("BeforeInitialization : " + beanName);
-		return bean; // podemos retornar um outro objeto qualquer tambem.
-	}
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException { // BeforeInitialization
+        // TODO Auto-generated method stub
+        System.out.println("BeforeInitialization : " + beanName);
+        return bean; // podemos retornar um outro objeto qualquer tambem.
+    }
 
-	@Override
-	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException { // AfterInitialization
-		// TODO Auto-generated method stub
-		System.out.println("AfterInitialization : "+ beanName);
-		return bean; // Podemos retornar um outro objeto qualquer tambem.
-	}
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException { // AfterInitialization
+        // TODO Auto-generated method stub
+        System.out.println("AfterInitialization : "+ beanName);
+        return bean; // Podemos retornar um outro objeto qualquer tambem.
+    }
 }
