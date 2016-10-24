a218d70458b1f66d1ffa2ab1061f44ed38d09ead
Daniel-Dos <daniel.dias.analistati@gmail.com>
2016-10-23 22:35:51 -0200

    Update - Descrição.

--- Spring-BeanLifeCycle/InitDestroyMethod/src/com/tutorialspoint/springframework/beanlifecycle/MainApp.java
+++ Spring-BeanLifeCycle/InitDestroyMethod/src/com/tutorialspoint/springframework/beanlifecycle/MainApp.java
@@ -4,6 +4,12 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
+ * 
+ * Se você estiver usando contêiner IoC do Spring em um ambiente de aplicação não-web; por exemplo,
+ *em um ambiente de desktop cliente rico, você registrar um gancho de encerramento com a JVM. fazendo
+ *assim garante um desligamento normal e chama o relevantes destruir métodos em seu singleton
+ *bean de modo que todos os recursos são liberados.
+ * 
  * Para registrar um shutdownhook usamos o metodo: {@link registerShutdownHook} que é declarado
  * quanto usamos a class {@link AbstractApplicationContext}. Ele é necessario para utilização
  * do shutdown e relevante para chamandas de metodos destroy.
