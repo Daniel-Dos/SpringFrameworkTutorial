ac7f800b4600fedca339a79ac519cee5fe3245d9
Daniel-Dos <daniel.dias.analistati@gmail.com>
2016-10-25 17:57:19 -0200

    Spring - Bean Definition Inheritance

---
+++ Spring-Bean Definition Inheritance/BeanDefinitionInheritance/src/com/tutorialspoint/springframework/beandefinitioninheritance/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.tutorialspoint.springframework.beandefinitioninheritance;
+
+/**
+ * @author daniel
+ * Daniel-Dos
+ * daniel.dias.analistati@gmail.com
+ */
+public class HelloWorld {
+	private String message;
+}
