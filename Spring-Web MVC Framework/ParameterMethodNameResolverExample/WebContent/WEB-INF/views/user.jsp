e3776238eb7ece1e48dd46501425c1764a3fd320
Daniel-Dos <daniel.dias.analistati@gmail.com>
2017-02-05 07:05:20 -0200

    Parameter Method Name Resolver.

---
+++ Spring-Web MVC Framework/ParameterMethodNameResolverExample/WebContent/WEB-INF/views/user.jsp
@@ -0,0 +1,11 @@
+<%@page contentType="text/html" language="java" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>${message }</title>
+</head>
+<body>
+		<h2>${message }</h2>
+</body>
+</html>
---
+++ Spring-Web MVC Framework/ParameterMethodNameResolverExample/src/com/tutorialspoint/mvc/UserController.java
@@ -0,0 +1,32 @@
+package com.tutorialspoint.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
+
+/**
+ * @author daniel github:Daniel-Dos daniel.dias.analistati@gmail.com
+ *         twitter:@danieldiasjava
+ */
+public class UserController extends MultiActionController {
+
+	public ModelAndView home(HttpServletRequest request,HttpServletResponse Response) {
+		ModelAndView model = new ModelAndView ("user");
+		model.addObject("message", "Home");
+		return model;
+	}
+
+	public ModelAndView add(HttpServletRequest request,HttpServletResponse Response) {
+		ModelAndView model = new ModelAndView ("user");
+		model.addObject("message", "Add");
+		return model;
+	}
+
+	public ModelAndView remove(HttpServletRequest request,HttpServletResponse Response) {
+		ModelAndView model = new ModelAndView ("user");
+		model.addObject("message", "Remove");
+		return model;
+	}
+}
