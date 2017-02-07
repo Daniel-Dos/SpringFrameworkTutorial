f9990491f7924efa331d848f2349954bdccbd4da
Daniel-Dos <daniel.dias.analistati@gmail.com>
2017-02-07 01:56:18 -0200

    Hibernate Validator - Run with Wildfly.

--- Spring-Web MVC Framework/HibernateValidatorExample/src/com/tutorialspoint/mvc/StudentController.java
+++ Spring-Web MVC Framework/HibernateValidatorExample/src/com/tutorialspoint/mvc/StudentController.java
@@ -1,10 +1,9 @@
 package com.tutorialspoint.mvc;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +29,7 @@ public class StudentController {
 	}
 
 	@PostMapping("/addStudent")
-	public String addStudent(@ModelAttribute("student") @Valid Student student,
+	public String addStudent(@ModelAttribute("student") @Validated Student student,
 	BindingResult bindingResult, Model model) {
 	if (bindingResult.hasErrors()) {
 	return "addStudent";
